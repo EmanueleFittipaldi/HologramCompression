f322dd06f59b38e5d87d1244013e556ac2100cb4
Emanuele Fittipaldi <e.fittipaldi@studenti.unisa.it>
2022-12-07 09:34:21 +0100

    Update main.py rimuovendo l'implementazione convoluzionale della diffrazione di Fresnel.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from givensphaserot import givenphaserot
 plt.rcParams["figure.figsize"] = (60,60)
 
 #Queste due funzioni servono per ricostruire l'ologramma.
-def intfresnel2D(x, fw, pp, z, wlen, algo):
+def intfresnel2D(x, fw, pp, z, wlen):
     # aggiungere un assert per controllare se l'array x non contiene zero
     assert (np.size(x) % 2) == 0, "La dimensione dell'ologramma deve essere pari"
     assert np.size(x, 0) == np.size(x, 1), "L'ologramma deve essere quadrato (da risolvere nelle prossime versioni)"
@@ -53,18 +53,11 @@ def intfresnel2D(x, fw, pp, z, wlen, algo):
             x[:, 1::2] = np.conj(o)
             return x
 
-    if algo == "conv":
-        fpropfun = confresnel1D.convfresnel1D  # convfresnel1D(x,pp,z,wlen)
-        bpropfun = confresnel1D.convfresnel1D  # convfresnel1D(x,pp,z,wlen)
-        fz = z
-        bz = -z
-    elif algo == "four":
-        fpropfun = fwdfourierfresnel1D
-        fz = z
-        bpropfun = revfourierfresnel1D
-        bz = z
-    else:
-        print("algoritmo sconosciuto")
+    #Applico la diffrazione di Fresnel
+    fpropfun = fwdfourierfresnel1D
+    fz = z
+    bpropfun = revfourierfresnel1D
+    bz = z
 
     for i in [-1, 1]:
         if fw:
@@ -87,7 +80,6 @@ def fourier_phase_multiply(r, fw, pp, z, wlen):
 
 def main():
     f = scipy.io.loadmat('Hol_2D_dice.mat')  # aprire il file .mat
-    #f = scipy.io.loadmat('exampleholo.mat')  # aprire il file .mat
 
     #Dice Parameters
     pp = 8e-6  # pixel pitch
@@ -97,65 +89,24 @@ def main():
     dist = 9e-1  # propogation depth
     dist = np.matrix(dist)
 
-    #Letter Parameter
-    #f = scipy.io.loadmat('exampleholo.mat')  # aprire il file .mat
-    #pp = 8e-6  # pixel pitch
-    #pp = np.matrix(pp)
-    #wlen = 532e-9  # wavelenght
-    #wlen = np.matrix(wlen)
-    #dist = 5e-1  # propogation depth
-    #dist = np.matrix(dist)
-
-    #Venus Parameters
-    #pp = 8e-6  # pixel pitch
-    #pp = np.matrix(pp)
-    #wlen = 632.8e-9  # wavelenght
-    #wlen = np.matrix(wlen)
-    #dist = 5e-1  # propogation depth
-    #dist = np.matrix(dist)
-
-    #Cat Parameters
-    #pp = 2e-6  # pixel pitch
-    #pp = np.matrix(pp)
-    #wlen = 632.8e-9  # wavelenght
-    #wlen = np.matrix(wlen)
-    #dist = 1e-1  # propogation depth
-    #dist = np.matrix(dist)
-
-
     #holo è la matrice di numeri complessi
     holo = np.matrix(f['Hol'])
 
-    #Prova con fpzip
-    #data = np.imag(holo)
-    #compressed_bytes = fzip.compress(data,precision=0,order='C')
-    #print(compressed_bytes)
-
-
-    #holo = np.matrix(f['holo'])
-
-    #Per gli ologrammi 1920*1080
+    #Effettuo un crop da 1920*1080 a 1080*1080 perché l'algoritmo per la
     holo = holo[:,420:]
     holo = holo[:,:-420]
 
+    #Per memorizzare la matrice
     #np.save("matriceComplessaCsingle.npy", np.csingle(holo))
-    #np.save("matriceImmaginaria.npy",np.imag(holo))
-    #np.save("matriceReale.npy",np.real(holo))
-
 
     #Per visualizzare la parte reale ed immaginaria dell'ologramma
     #plt.imshow(np.imag(holo), cmap="gray")
     #plt.show()  # mostro l'ologramma pre - compressione
-
     #plt.imshow(np.real(holo), cmap="gray")
     #plt.show()
 
-    #Scegliere l'implementazione di Fresnel
-    #algo='conv'
-    algo = 'four'
-
     #Ricostruzione dell'ologramma tramite diffrazione di Fresnel
-    t = intfresnel2D(np.csingle(holo), False, pp, dist, wlen, algo)
+    t = intfresnel2D(np.csingle(holo), False, pp, dist, wlen)
     t = fourier_phase_multiply(t, False, pp, dist, wlen)
     plt.imshow(np.imag(t), cmap='gray')
     plt.show()
